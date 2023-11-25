a09a208d502b4e2939e7fd8bbd0b8524443d5080
Charchit <charchitbhatnagar@gmail.com>
2023-11-26 03:38:15 +0530

    Commit

--- server/firebase.js
+++ server/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 require('dotenv').config();
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId
+  apiKey: "AIzaSyCx_h9HmjAllyRDppdTi9RUz-JutidPjls",
+  authDomain: "todo-5e701.firebaseapp.com",
+  projectId: "todo-5e701",
+  storageBucket: "todo-5e701.appspot.com",
+  messagingSenderId: "488320199317",
+  appId: "1:488320199317:web:cdfbe6aa1114142551499c",
+  measurementId: "G-L4JMV347GR"
 };
 
 // Initialize Firebase
